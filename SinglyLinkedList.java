a4d1e4c743da4fb010acdbfb2de39b3f4a1a1724
Aaron <33184112+astark929@users.noreply.github.com>
2022-10-30 22:31:51 -0700

    Add files via upload

---
+++ SinglyLinkedList.java
@@ -0,0 +1,66 @@
+package Recursion;
+
+class SinglyLinkedList<T>
+{
+    private int currentLength = 0;
+
+    class Node<T>
+    {
+        T data;
+        Node next;
+    }
+
+    public Node remove(Node head, int k)
+    {
+
+        Node returnNode = head;
+        Node current = new Node();
+        Node previous = head;
+
+        int count = 0;
+
+        while(count < currentLength){
+            if(count == k-1){
+                current = previous.next;
+                break;
+            }
+            else{
+                previous = previous.next;
+                count++;
+            }
+        }
+        previous.next = current.next;
+        return returnNode;
+    }
+
+    public Node push(Node head, T new_data)
+    {
+        Node node = new Node();
+        node.data = new_data;
+
+        if(head == null){
+            return node;
+        }
+        else {
+            node.next = head;
+            head = node;
+        }
+
+        currentLength++;
+
+        return head;
+    }
+
+    public String toString(Node head)
+    {
+        Node position = head;
+
+        String str = "";
+        while(position != null){
+            str += position.data + " ";
+            position = position.next;
+        }
+
+        return str;
+    }
+}
